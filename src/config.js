60cbfe7f8961e0de5491e0ae2a898e4671db4524
root <root@riceapps.org>
2018-04-05 03:03:27 +0000

    add https to config

--- src/config.js
+++ src/config.js
@@ -6,8 +6,8 @@ let CONFIG = function() {
       secret: 'da427f7f3d4070822370352f976a7e9f26ee2590127d7dd5a4be1b0b9e647f29',
       db_uri: 'mongodb://riceapps:r1ce4pps$wag1@ds121945.mlab.com:21945/ricebikes-dev',
       CASValidateURL: 'https://idp.rice.edu/idp/profile/cas/serviceValidate',
-      CASthisServiceURL: 'http://bikes.riceapps.org/auth',
-      frontendURL: 'http://bikes.riceapps.org',
+      CASthisServiceURL: 'https://bikes.riceapps.org/auth',
+      frontendURL: 'https://bikes.riceapps.org',
       email: {
         user: 't4jves3pwjtt2svq@ethereal.email',
         pass: 'mPsJ9XKegskYgEaqZr',
