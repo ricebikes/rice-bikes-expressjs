8593ba255de238bceb0cd805770809f77641e5cb
Daniel DeGrasse <daniel@degrasse.com>
2019-10-26 23:41:26 -0500

    Implemented most of fields in Item model

--- src/models/Item.js
+++ src/models/Item.js
@@ -1,14 +1,14 @@
 var mongoose = require('mongoose');
 
 var ItemSchema = new mongoose.Schema({
+  supplier: String,
+  upc : Number,
   category: String,
-  size: String,
   name: {type:String,required:true},
   description: String,
   standard_price: {type:Number,required:true},
   wholesale_cost:Number,
-  quantity:{type:Number,default:0},
-  warning_quantity:Number
+  stock:{type:Number,default:0},
 });
 // text index lets us search by name
 ItemSchema.index({name:'text'});
