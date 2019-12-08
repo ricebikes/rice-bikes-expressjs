a3d7abb2455feac8266cb8a77edf817c7c2b1670
Daniel DeGrasse <daniel@degrasse.com>
2019-12-08 00:59:37 -0600

    Implemented PUT method to update orders

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -33,11 +33,36 @@ router.get('/daterange',function (req,res) {
  * GET: /. Alias to GET /daterange (gets all orders)
  */
 router.get('/', function (req, res) {
-    res.redirect("/daterange");
+    res.redirect("./daterange");
 });
 
 // require admin permissions to use the below endpoints
 router.use(adminMiddleware);
+
+/**
+ * Utility function used by POST and PUT to resolve a list of item IDs to items in the database
+ * @param item: List of items, in the following format:
+ *  [ {item: Item, quantity: Number transaction(optional): Transaction } ]
+ * @return Array of promises, which can be resolved to mongodb item objects
+ */
+async function resolveItems(item) {
+    let itemRef = await Item.findById(item.item._id);
+    if (!itemRef) {
+        // throw error
+        throw {err: "Item not found!"};
+    }
+    if (item.transaction) {
+        // populate transaction ref
+        let transactionRef = await Transaction.findById(item.transaction._id);
+        if (!transactionRef) {
+            // throw error
+            throw {err: "Transaction was not found"};
+        }
+        return {item: itemRef, transaction: transactionRef, quantity: item.quantity};
+    } else {
+        return {item: itemRef, quantity: item.quantity};
+    }
+}
 
 /**
  * POSTs a new order
@@ -55,28 +80,11 @@ router.post('/',async (req, res) => {
         return res.status(400).send("No supplier provided");
     }
     try {
-        let supplier = req.body.supplier;
+        const supplier = req.body.supplier;
         // for each item populate the reference and any transaction referenced. This function will return promises.
-        let itemPromises = req.body.items.map(async item => {
-            let itemRef = await Item.findById(item.item._id);
-            if (!itemRef) {
-                // throw error
-                throw {err: "Item not found!"};
-            }
-            if (item.transaction) {
-                // populate transaction ref
-                let transactionRef = await Transaction.findById(item.transaction._id);
-                if (!transactionRef) {
-                    // throw error
-                    throw {err: "Transaction was not found"};
-                }
-                return {item: itemRef, transaction: transactionRef, quantity: item.quantity};
-            } else {
-                return {item: itemRef, quantity: item.quantity};
-            }
-        });
+        const itemPromises = req.body.items.map(resolveItems);
         // wait synchronously until all items are populated
-        let populatedItems = await Promise.all(itemPromises);
+        const populatedItems = await Promise.all(itemPromises);
         // create order using populated item refs
         let newOrder = await Order.create({supplier: supplier, date_created: new Date(), items: populatedItems});
         res.status(200).send(newOrder);
@@ -92,15 +100,36 @@ router.post('/',async (req, res) => {
  * Item array will be overwritten
  * put body:
  * {
+ *   supplier: String
  *   tracking_number: Number
  *   items : [ {item: Item, quantity: Number} ]
+ *   status: String
  * }
  */
-router.put('/:id',function (req,res) {
-    Order.findByIdAndUpdate(req.params.id, req.body, function (err, order) {
-        if (err) return res.status(500).send(err);
-        return res.status(200).send(order);
-    })
+router.put('/:id',async (req,res) => {
+    try {
+        let order = await Order.findById(req.params.id);
+        if (!order) return res.status(404).send("No order found!");
+        // conditionally update each portion of the order, based on if it is present
+        if (req.body.tracking_number) {
+            order.tracking_number = req.tracking_number;
+        }
+        if (req.body.items) {
+            const promises = req.body.items.map(resolveItems);
+            order.items = await Promise.all(promises);
+        }
+        if (req.body.status) {
+            order.status = req.body.status;
+        }
+        if (req.body.supplier) {
+            order.supplier = req.body.supplier;
+        }
+        // save new order into DB, and return the result
+        const updatedOrder = await order.save();
+        return res.status(200).send(updatedOrder);
+    } catch (err) {
+        return res.status(500).send(err);
+    }
 });
 
 module.exports = router;
--- src/models/Order.js
+++ src/models/Order.js
@@ -4,6 +4,7 @@ var OrderSchema = new mongoose.Schema({
     supplier: {type: String, required: true},
     date_created: {type: Date, required: true},
     tracking_number: Number,
+    status: String,
     items: [{item: {type: mongoose.Schema.Types.ObjectId, ref: 'Item'},
         quantity: Number, transaction: {type: mongoose.Schema.Types.ObjectId, ref: 'Transaction', required: false}}]
 });
